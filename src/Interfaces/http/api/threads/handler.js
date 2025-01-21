08b689357f2b597a4874f5777ff6a0a17a611948
AchmadLyraa <89878825+AchmadLyraa@users.noreply.github.com>
2025-01-21 18:14:12 +0800

    Add files via upload

---
+++ src/Interfaces/http/api/threads/handler.js
@@ -0,0 +1,57 @@
+const AddThreadUseCase = require("../../../../Applications/use_case/AddThreadUseCase");
+const GetThreadDetailsUseCase = require("../../../../Applications/use_case/GetThreadDetailsUseCase");
+
+class ThreadsHandler {
+	constructor(container) {
+		this._container = container;
+		this.postThreadHandler = this.postThreadHandler.bind(this);
+		this.getThreadsHandler = this.getThreadsHandler.bind(this);
+	}
+
+	async postThreadHandler(request, h) {
+		const { id: owner } = request.auth.credentials;
+		const { title, body } = request.payload;
+		const addThreadUseCase = this._container.getInstance(AddThreadUseCase.name);
+		const addedThread = await addThreadUseCase.execute({ title, body, owner });
+
+		const response = h.response({
+			status: "success",
+			data: {
+				addedThread,
+			},
+		});
+		response.code(201);
+		return response;
+	}
+
+	async getThreadsHandler(request, h) {
+		const { threadId } = request.params;
+		const getThreadDetailsUseCase = this._container.getInstance(
+			GetThreadDetailsUseCase.name
+		);
+		try {
+			const threadDetails = await getThreadDetailsUseCase.execute(threadId);
+
+			return h
+				.response({
+					status: "success",
+					data: {
+						thread: threadDetails,
+					},
+				})
+				.code(200);
+		} catch (error) {
+			if (error.message === "THREAD_NOT_FOUND") {
+				return h
+					.response({
+						status: "fail",
+						message: "Thread tidak ditemukan",
+					})
+					.code(404);
+			}
+			throw error;
+		}
+	}
+}
+
+module.exports = ThreadsHandler;
